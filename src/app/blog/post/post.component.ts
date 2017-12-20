f3a7d3db2524e42c2406ae495c5c2de3dab4c8a5
Brenden <brenden.niedermeyer@gmail.com>
2017-12-20 13:50:39 -0800

    actually add new files

---
+++ src/app/blog/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import * as marked from 'marked';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
