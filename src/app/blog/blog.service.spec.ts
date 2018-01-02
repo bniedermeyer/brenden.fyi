1903796f36212a6ac113cc0dfc93904659e4c94e
Brenden <brenden.niedermeyer@gmail.com>
2018-01-02 15:19:32 -0800

    update blog test

--- src/app/blog/blog.service.spec.ts
+++ src/app/blog/blog.service.spec.ts
@@ -1,27 +1,36 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { afterEach } from 'selenium-webdriver/testing';
 
 import { BlogService } from './blog.service';
+import { BlogPost } from './post/post.model';
 
 describe('BlogService', () => {
+  let injector: TestBed;
+  let service: BlogService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [BlogService]
     });
+    injector = getTestBed();
+    service = injector.get(BlogService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  fit(
-    'should be created',
-    inject([BlogService], (service: BlogService) => {
-      expect(service).toBeTruthy();
-    })
-  );
+  afterEach(() => {
+    httpMock.verify();
+  });
 
-  it(
-    'should return the blog object',
-    inject([BlogService], (service: BlogService) => {
-      service.parseBlogPost('test').subscribe(data => {
-        expect(data).toBeTruthy();
-      });
-    })
-  );
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return an observable of the blog post', () => {
+    service.parseBlogPost('test').subscribe(blog => {
+      expect(true).toBeTruthy();
+    });
+  });
 });
